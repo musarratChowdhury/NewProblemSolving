86039f0f152d99990924646f1db9e113ec6ca227
musarratchowdhury <muhitlopez143@gmail.com>
2022-01-05 01:01:10 +0600

    solved

--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -11,9 +11,26 @@ namespace ProblemSolving
 		public static void Main( string[] args )
 		{
 			//input
-			var line1 = Console.ReadLine();
+			//var line1 = Console.ReadLine();
 			///input a number
-			///var n = Convert.ToInt32(Console.Readline());
+			var n = Console.ReadLine();
+			char[] chars  = n.ToCharArray();	
+
+			var luckyDigitCount = 0;
+			for( int i = 0; i < chars.Length; i++ )
+			{
+				if( chars[i] is '4' || chars[i] is '7')
+				{
+					luckyDigitCount++;				
+				}
+			}
+			if(luckyDigitCount!=7&&luckyDigitCount!=4)
+			Console.WriteLine("NO");
+			else
+				Console.WriteLine("YES");
+			
+
+
 		}
 	}
 }
